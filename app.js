0b6de0777cac69c86ec7541a607fcb3f0ab4efed
boontawatk <boontawat.k@gmail.com>
2020-10-26 20:04:50 +0700

    add show all campground page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground = require("./models/campground")
+const Campground = require("./models/campground");
 
 mongoose
   .connect("mongodb://localhost:27017/yelpCamp", {
     useNewUrlParser: true,
-    useCreateIndex: true,//I have no idea why we need this
-    useUnifiedTopology: true
+    useCreateIndex: true, //I have no idea why we need this
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to yelpCamp database!"))
   .catch((error) => console.log(error.message));
@@ -20,6 +20,12 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
 
 app.listen(3000, () => {
   console.log("Server on Port 3000");
