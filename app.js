bf2b4d8d95c8601021f8a49cd6c204aa0186984d
boontawatk <boontawat.k@gmail.com>
2020-11-13 11:16:55 +0700

    add validation in 'new'

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const { nextTick } = require("process");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 
 mongoose
   .connect("mongodb://localhost:27017/yelpCamp", {
@@ -25,6 +26,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 //use for custom css
 app.use(express.static(__dirname + "/views"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //can use other word instead_method -> _method is parameter in query string
 app.use(methodOverride("_method"));
@@ -49,8 +51,26 @@ app.get("/campgrounds/new", async (req, res, next) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError("Invalid Campground Data", 400);
+    console.log(req.body);
+    // if (!req.body.campground)
+    //   throw new ExpressError("Invalid Campground Data", 400);
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().min(0).required(),
+        image: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+      }).required(),
+    });
+    //what's inside is what we want to pass data for validation
+    //we want to pass req.body -> so we want to validate campground too
+    //if we want just validate what's inside it we can pass req.body.campground and what's inside for schema
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`);
